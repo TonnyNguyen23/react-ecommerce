89e495ef5c44d155e70aad17cfa9d30982d50fa5
Tonny <nguyenxuantung473@gmail.com>
2022-03-11 12:27:15 +0700

    [tungnx] create UI profile page

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,61 +1,65 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { Nav, Container, Navbar, NavDropdown } from 'react-bootstrap'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Nav, Container, Navbar, NavDropdown } from "react-bootstrap";
 
 export const Header = () => {
-  const carts = useSelector(state => state.carts)
+  const carts = useSelector((state) => state.carts);
   // console.log(carts);
   return (
     <header>
       <Navbar
-        bg='dark'
-        expand='lg'
-        variant='dark'
+        bg="dark"
+        expand="lg"
+        variant="dark"
         collapseOnSelect
-        className='shadow-sm'
-        fixed='top'
-        style={{ height: '80px' }}
+        className="shadow-sm"
+        fixed="top"
+        style={{ height: "80px" }}
       >
         <Container>
-          <Navbar.Brand className='fw-bold'>Online Shop</Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='d-flex w-100 '>
+          <Navbar.Brand className="fw-bold">
+            <Nav.Link className="text-light" as={NavLink} to="/">
+              Online Shop
+            </Nav.Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="d-flex w-100 ">
               {/* Menu page */}
-              <div className='flex-grow-1 d-flex justify-content-center'>
-                <Nav.Link as={NavLink} to='/'>
+              <div className="flex-grow-1 d-flex justify-content-center">
+                <Nav.Link as={NavLink} to="/">
                   Home
                 </Nav.Link>
-                <Nav.Link as={NavLink} to='/products'>
+                <Nav.Link as={NavLink} to="/products">
                   Products
                 </Nav.Link>
-                <Nav.Link as={NavLink} to='/about'>
+                <Nav.Link as={NavLink} to="/about">
                   About
                 </Nav.Link>
-                <Nav.Link as={NavLink} to='/contact'>
+                <Nav.Link as={NavLink} to="/contact">
                   Contact
                 </Nav.Link>
               </div>
 
               {/* Buttons  */}
-              <div className='d-flex'>
-                <Nav.Link as={NavLink} to='/cart'>
-                  <i className='fa fa-shopping-cart me-1'></i>Cart (
+              <div className="d-flex">
+                <Nav.Link as={NavLink} to="/cart">
+                  <i className="fa fa-shopping-cart me-1"></i>Cart (
                   {carts.length})
                 </Nav.Link>
 
-                <Nav.Link as={NavLink} to='/login'>
+                <Nav.Link as={NavLink} to="/login">
                   Login
-                  <i class='fa-solid fa-user' />
+                  <i class="fa-solid fa-user" />
                 </Nav.Link>
 
                 {false && (
-                  <NavDropdown title='minh' id='username'>
-                    <NavDropdown.Item to='/profile'>Profile</NavDropdown.Item>
+                  <NavDropdown title="minh" id="username">
+                    <NavDropdown.Item to="/profile">Profile</NavDropdown.Item>
 
                     <NavDropdown.Item
-                      onClick={() => console.log('logoutHandler')}
+                      onClick={() => console.log("logoutHandler")}
                     >
                       Logout
                     </NavDropdown.Item>
@@ -63,17 +67,17 @@ export const Header = () => {
                 )}
               </div>
 
-              {false && false === 'admin' && (
-                <NavDropdown title='Admin' id='adminmenu'>
-                  <NavDropdown.Item to='/admin/userlist'>
+              {false && false === "admin" && (
+                <NavDropdown title="Admin" id="adminmenu">
+                  <NavDropdown.Item to="/admin/userlist">
                     Users
                   </NavDropdown.Item>
 
-                  <NavDropdown.Item to='/admin/productlist'>
+                  <NavDropdown.Item to="/admin/productlist">
                     Products
                   </NavDropdown.Item>
 
-                  <NavDropdown.Item to='/admin/orderlist'>
+                  <NavDropdown.Item to="/admin/orderlist">
                     Orders
                   </NavDropdown.Item>
                 </NavDropdown>
@@ -83,5 +87,5 @@ export const Header = () => {
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
